00b7be9cf497ede7f25b070fc667d6b49a505c42
Souvlakis <soouvlaki@yandex.ru>
2023-01-10 22:06:15 +0200

    Added loader api

---
+++ src/main/java/top/polar/api/loader/LoaderApi.java
@@ -0,0 +1,9 @@
+package top.polar.api.loader;
+
+public class LoaderApi {
+
+    public static void registerEnableCallback(Runnable runnable) {
+        throw new IllegalStateException("Not implemented");
+    }
+
+}
